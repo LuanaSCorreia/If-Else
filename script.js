02961e78b5e2e8dafe4a316e63d67b840ce5b1d0
LuanaSCorreia <luanacorreia797@gmail.com>
2022-06-13 09:45:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+/*exercício 1*/
+{let idade = 22
+if (idade>18){
+console.log("Parabês, você já pode ser preso")
+}else
+{
+console.log("Infelizmente você ainda é uma criança") 
+}
+}
+/*Exercício 2 */
+{let idade = 16
+let humano = true
+
+if (idade>18 && humano){
+console.log("Você é humano e maior de idade. Acertei?")
+}else
+{
+console.log("Você talvez seja o baby Yoda")
+}
+}
+
+/*Exercício 3 */
+{let mes = "janeiro"
+
+if( mes == "janeiro" || mes == "dezembro"){
+console.log("Você é capricorniana, estou chegando perto?")
+}else
+{
+console.log("Ainda não descobri seu aniversário mas vou conseguir")
+}
+}
+
+/*Exercício 4 
+Não deu tempo de achar esse :)
+*/
+
+/*Exercício 5 */
+{
+let sobrenome= "correia"
+let nome = ""
+if( sobrenome.length>6 || nome){
+console.log("Deixa eu adivinhar...Seu nome começa com R e seu sobrenome tem mais de 6 letras. Acertei?")
+}else
+{
+console.log("Poxa... Estou chegando perto?")
+}
+}
